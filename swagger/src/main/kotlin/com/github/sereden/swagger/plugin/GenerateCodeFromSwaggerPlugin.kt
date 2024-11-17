82662afe3ac6771d0f251b73c5f55a31192436ae
Andriy Seredenko <a.seredenko@pri-num.com>
2024-11-17 15:34:43 +0200

    Fixed the tasks is not invoked during app assembling

--- swagger/src/main/kotlin/com/github/sereden/swagger/plugin/GenerateCodeFromSwaggerPlugin.kt
+++ swagger/src/main/kotlin/com/github/sereden/swagger/plugin/GenerateCodeFromSwaggerPlugin.kt
@@ -3,14 +3,12 @@ package com.github.sereden.swagger.plugin
 import com.github.sereden.swagger.processor.CodeGeneratorProcessor
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.withType
-import org.gradle.language.assembler.tasks.Assemble
 
 class GenerateCodeFromSwaggerPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         val extension = target.extensions.create("swagger", SwaggerExtensions::class.java)
-        target.tasks.register("generateCodeFromSwaggerTask") {
+        val generateTask = target.tasks.register("generateCodeFromSwaggerTask") {
             group = "Code generation"
             description = "Generates Kotlin data classes from Swagger API schema"
 
@@ -18,8 +16,11 @@ class GenerateCodeFromSwaggerPlugin : Plugin<Project> {
                 CodeGeneratorProcessor(target, extension).process()
             }
         }
-        target.tasks.withType<Assemble>{
-            dependsOn("generateCodeFromSwaggerTask")
+
+        target.tasks.configureEach {
+            if (name.startsWith("assemble")) {
+                dependsOn(generateTask)
+            }
         }
     }
 }
