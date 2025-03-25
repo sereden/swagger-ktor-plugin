1e8893dcc5fcec50ce049c54529d026ae81f58a4
Andriy Seredenko <a.seredenko@pri-num.com>
2025-03-25 20:15:03 +0200

    Implemented github publishing script

--- swagger/build.gradle.kts
+++ swagger/build.gradle.kts
@@ -18,11 +18,27 @@ tasks.test {
     useJUnitPlatform()
 }
 
+version = "1.0.0"
+group = "io.github.sereden"
+
 gradlePlugin {
     plugins {
-        register("generateCodeFromSwagger") {
-            id = "io.github.sereden.swagger"
+        create("generateCodeFromSwagger") {
+            id = "io.github.sereden.swagger-ktor-plugin"
             implementationClass = "com.github.sereden.swagger.plugin.GenerateCodeFromSwaggerPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/sereden/swagger-ktor-plugin")
+            credentials {
+                username = project.findProperty("GITHUB_USER") as? String ?: System.getenv("GITHUB_USER")
+                password = project.findProperty("GITHUB_TOKEN") as? String ?: System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
